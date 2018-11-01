c1844de7f367228f38a68eb0031e7382ad3fdada
v.jitendra.munjani <v.jitendra.munjani@IGECO-DEV.AWSAPPS.COM>
2018-11-01 10:50:16 +0530

    Edit app.js - remove useMongoClient option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'todo-client';
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+
+    this.todoService.getToDos().subscribe(data => {
+      console.log(data);
+    })
+
+  }
+
 }
